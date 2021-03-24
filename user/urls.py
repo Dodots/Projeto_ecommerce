00e822a57accf59616f14e7b94e4cae59c010a29
Douglas <doouglascarvalho92@gmail.com>
2021-03-24 15:11:07 -0300

    Style: colocando a url do update do usuário

--- user/urls.py
+++ user/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name = "logout"),
     path('register/', UsuarioCreateView.as_view(), name="register"),
-    path('update-data/', PerfilUpdateView.as_view(), name='update-data')
+    path('update_data/', PerfilUpdateView.as_view(), name='update_data')
 ]
