506d5c223365ce01fc282132ad7fff2e278ef646
Douglas <doouglascarvalho92@gmail.com>
2021-04-11 10:19:01 -0300

    Feacture: criando template do create e update de sucesso do user

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import UsuarioCreateView, PerfilUpdateView
+from .views import UsuarioCreateView, PerfilUpdateView, CreateSuccess, UpdateSuccess
+from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 
 app_name = "user"
@@ -9,5 +10,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name = "logout"),
     path('register/', UsuarioCreateView.as_view(), name="register"),
-    path('update_data/', PerfilUpdateView.as_view(), name='update_data')
+    path('update_data/', PerfilUpdateView.as_view(), name='update_data'),
+    path('create_success', CreateSuccess.as_view(), name='create_user_success'),
+    path('update_success', UpdateSuccess.as_view(), name='update_user_success'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.base import TemplateView
 from django.contrib.auth.models import User, Group
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
@@ -6,10 +7,11 @@ from django.shortcuts import get_object_or_404
 
 from .models import Perfil
 
+
 class UsuarioCreateView(CreateView):
     template_name = "user/form.html"
     form_class = UsuarioForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('user:create_user_success')
 
     def form_valid(self, form):
 
@@ -24,12 +26,30 @@ class UsuarioCreateView(CreateView):
 
         return url
 
+
 class PerfilUpdateView(UpdateView):
     template_name = "user/form.html"
     model = Perfil
     fields = ["nome_completo", "cpf", "telefone"]
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("user:update_user_success")
 
     def get_object(self):
         self.get_object = get_object_or_404(Perfil, usuario=self.request.user)
         return self.get_object
+
+
+class CreateSuccess(TemplateView):
+    template_name = "user/create_user_success.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['status_conta'] = 'Sua conta foi criada com sucesso!'
+        return context
+
+class UpdateSuccess(TemplateView):
+    template_name = "user/create_user_success.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['status_conta'] = 'Sua conta foi atualizada com sucesso!'
+        return context
